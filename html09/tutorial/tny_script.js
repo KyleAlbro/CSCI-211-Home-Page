db5f0186b33cce655d6020dd45ad5b2ec7dad3e6
KyleAlbro <kylealbro@charter.net>
2022-10-25 10:56:27 -0600

    changes

--- html09/tutorial/tny_script.js
+++ html09/tutorial/tny_script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
@@ -7,5 +8,11 @@
    Author: Kyle Albro
    Date:   10/25/22
 
+*/
+
+window.alert("Welcome to Tulsa");
+/*
+this is an error
+Window.Alert("Welcome to Tulsa");
 */
 
