6944d517d88817edfe7080f74928247106621c2c
KyleAlbro <kylealbro@charter.net>
2022-11-06 11:11:59 -0700

    i think im done

--- html10/case2/hg_report.js
+++ html10/case2/hg_report.js
@@ -14,7 +14,7 @@
 
 var gameReport =
    '<h1>' + itemTitle + '</h1><h2>By: ' + itemManufacturer + '</h2>' +
-   '<img src="images/hg_' + itemID + '.png" alt="' + itemID + 'gameImg" />' +
+   '<img src="hg_' + itemID + '.png" alt="' + itemID + 'gameImg" />' +
    '<table><tr><th>Product ID</th><td>' + itemID + '</td></tr>' +
    '<tr><th>List Price</th><td>' + itemPrice + '</td></tr>' +
    '<tr><th>Platform</th><td>' + itemPlatform + '</td></tr>' +
@@ -37,14 +37,13 @@ var ratingReport =
    ' out of 5 stars (' + ratingsCount + ' reviews)</h2>';
 
 for (var i = 0; i < 3; i++) {
-   ratingReport +=
-      '<div class="review"><h1>' + ratingTitles[i] + '</h1>' +
+   ratingReport += '<div class="review"><h1>' + ratingTitles[i] + '</h1>' +
       '<table><tr><th>By</th><td>' + ratingAuthors[i] + '</td></tr>' +
       '<tr><th>Review Date</th><td>' + ratingDates[i] + '</td></tr>' +
       '<tr><th>Rating</th><td>';
    // nested for loop to represent each review's rating with stars
    for (var j = 1; j <= ratings[i]; j++) {
-      ratingReport += '<img src="images/hg_star.png" />';
+      ratingReport += '<img src="hg_star.png" />';
    }
    ratingReport += '</td></tr></table>' + ratingSummaries[i] + '</div>';
 }
