938398f1f57dfcdaaae29a787a1ae0f33f4f0d2b
KyleAlbro <kylealbro@charter.net>
2022-11-10 11:48:09 -0700

    ch 11 tutorial finished

--- html11/tutorial/jpf_hanjie.js
+++ html11/tutorial/jpf_hanjie.js
@@ -58,60 +58,117 @@ window.onload = init;
 var puzzleCells;
 var cellBackground;
 
-function init(){
+function init() {
    document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
 
-   document.getElementById("puzzle").innerHTML = 
-   drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+   document.getElementById("puzzle").innerHTML =
+      drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
 
    var puzzleButtons = document.getElementsByClassName("puzzles");
-   for (var  i= 0; i< puzzleButtons.length; i++){
+   for (var i = 0; i < puzzleButtons.length; i++) {
       puzzleButtons[i].onclick = swapPuzzle;
    }
 
    setupPuzzle();
 
    document.addEventListener("mouseup", endBackground);
+
+   document.getElementById("solve").addEventListener("click", function () {
+      for (var i = 0; i < puzzleCells.length; i++) {
+         puzzleCells[i].style.backgroundColor = "";
+      }
+   });
 }
 
-function swapPuzzle(e){
-   var puzzleID = e.target.id;
+function swapPuzzle(e) {
+   if (confirm("You will lose all of your work on the puzzle! Continue?")) {
+      var puzzleID = e.target.id;
 
-   var puzzleTitle = e.target.value;
-   document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
+      var puzzleTitle = e.target.value;
+      document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
 
-   switch (puzzleID) {
-      case "puzzle1":
-         document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
-         break;
-      case "puzzle2":
-         document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
-         break;
-      case "puzzle3":
-         document.getElementById("puzzle").innerHTML =drawPuzzle (puzzle3Hint, puzzle3Rating, puzzle3);
-         break;
+      switch (puzzleID) {
+         case "puzzle1":
+            document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+            break;
+         case "puzzle2":
+            document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
+            break;
+         case "puzzle3":
+            document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
+            break;
+      }
+
+      setupPuzzle();
    }
 
-   setupPuzzle();
 }
 
-function setupPuzzle(){
-   puzzleCells =document.querySelectorAll("table#hanjieGrid td");
+function setupPuzzle() {
+   puzzleCells = document.querySelectorAll("table#hanjieGrid td");
 
-   for (var i = 0; i < puzzleCells.length; i++){
+   for (var i = 0; i < puzzleCells.length; i++) {
       puzzleCells[i].style.backgroundColor = "rgb(233, 207, 29)";
       puzzleCells[i].onmousedown = setBackground;
       puzzleCells[i].style.cursor = "url(jpf_pencil.png), pointer";
    }
+
+   document.getElementById("hanjieGrid").addEventListener("mouseup",
+      function() {
+         var solved = true;
+         for (var i = 0; i < puzzleCells.length; i++) {
+            if ((puzzleCells[i].className === "filled" &&
+                 puzzleCells[i].style.backgroundColor !== "rgb(101, 101, 101)")
+               ||
+               (puzzleCells[i].className === "empty" &&
+                  puzzleCells[i].style.backgroundColor === "rgb(101, 101, 101)")) {
+               solved = false;
+               break;
+            }
+         }
+         if (solved) alert("You Solved the Puzzle");
+
+      }
+   );
+   var filled = document.querySelectorAll("table#hanjieGrid td.filled");
+   var empty = document.querySelectorAll("table#hanjieGrid td.empty");
+
+   document.getElementById("peek").addEventListener("click",
+      function () {
+         for (var i = 0; i < filled.length; i++) {
+            if (filled[i].style.backgroundColor === "rgb(255, 255, 255)") {
+               filled[i].style.backgroundColor = "rgb(255, 211, 211)";
+            }
+         }
+
+         for (var i = 0; i < empty.length; i++) {
+            if (empty[i].style.backgroundColor === "rgb(101, 101, 101)") {
+               empty[i].style.backgroundColor = "rgb(255, 101, 101)";
+            }
+         }
+
+         setTimeout(
+            function () {
+               for (var i = 0; i < puzzleCells.length; i++) {
+                  if (puzzleCells[i].style.backgroundColor === "rgb(255, 211, 211)") {
+                     puzzleCells[i].style.backgroundColor = "rgb(255, 255, 255)";
+                  }
+                  if (puzzleCells[i].style.backgroundColor === "rgb(255, 101, 101)") {
+                     puzzleCells[i].style.backgroundColor = "rgb(101, 101, 101)";
+                  }
+               }
+            }, 500);
+      }
+   );
 }
 
-function setBackground(e){
+function setBackground(e) {
    var cursorType;
    if (e.shiftKey) {
       cellBackground = "rgb(233, 207, 29)";
       cursorType = "url(jpf_eraser.png), cell";
    }
-   else if (e.altKey){
+   else if (e.altKey) {
       cellBackground = "rgb(255, 255, 255)";
       cursorType = "url(jpf_cross.png), crosshair";
    }
@@ -121,7 +178,7 @@ function setBackground(e){
    }
    e.target.style.backgroundColor = cellBackground;
 
-   for(var i = 0; i < puzzleCells.length; i++){
+   for (var i = 0; i < puzzleCells.length; i++) {
       puzzleCells[i].addEventListener("mouseenter", extendBackground);
       puzzleCells[i].style.cursor = cursorType;
    }
@@ -129,12 +186,12 @@ function setBackground(e){
    e.preventDefault();
 }
 
-function extendBackground(e){
+function extendBackground(e) {
    e.target.style.backgroundColor = cellBackground;
 }
 
-function endBackground(){
-   for (var i = 0; i< puzzleCells.length; i++){
+function endBackground() {
+   for (var i = 0; i < puzzleCells.length; i++) {
       puzzleCells[i].removeEventListener("mouseenter", extendBackground);
       puzzleCells[i].style.cursor = "url(jpf_pencil.png), pointer";
    }
@@ -143,7 +200,7 @@ function endBackground(){
 /* ================================================================= */
 
 function drawPuzzle(hint, rating, puzzle) {
-   
+
    /* Initial HTML String for the Hanjie Puzzle */
    var htmlString = "";
 
@@ -162,21 +219,21 @@ function drawPuzzle(hint, rating, puzzle) {
    var rowCount = [];
    var spaceCount;
    for (var i = 0; i < totalRows; i++) {
-      rowCount[i]="";
+      rowCount[i] = "";
       spaceCount = 0;
 
       for (var j = 0; j < totalCols; j++) {
          if (puzzle[i][j] === "#") {
             spaceCount++;
-            if (j === totalCols-1) {
+            if (j === totalCols - 1) {
                rowCount[i] += spaceCount + "&nbsp;&nbsp;";
             }
          } else {
             if (spaceCount > 0) {
                rowCount[i] += spaceCount + "&nbsp;&nbsp;";
                spaceCount = 0;
-            } 
-         }    
+            }
+         }
       }
 
    }
@@ -186,21 +243,21 @@ function drawPuzzle(hint, rating, puzzle) {
 
    var colCount = [];
    for (var j = 0; j < totalCols; j++) {
-      colCount[j]="";
+      colCount[j] = "";
       spaceCount = 0;
 
       for (var i = 0; i < totalRows; i++) {
          if (puzzle[i][j] === "#") {
             spaceCount++;
-            if (i === totalRows-1) {
+            if (i === totalRows - 1) {
                colCount[j] += spaceCount + "<br />";
             }
          } else {
             if (spaceCount > 0) {
                colCount[j] += spaceCount + "<br />";
                spaceCount = 0;
-            } 
-         }    
+            }
+         }
       }
 
    }
@@ -220,9 +277,9 @@ function drawPuzzle(hint, rating, puzzle) {
    htmlString += "</tr>";
 
    for (var i = 0; i < totalRows; i++) {
-      htmlString += "<tr><th class='rows'>&nbsp;" + rowCount[i]+"</th>";
+      htmlString += "<tr><th class='rows'>&nbsp;" + rowCount[i] + "</th>";
 
-      for (var j = 0; j<totalCols; j++) {
+      for (var j = 0; j < totalCols; j++) {
          if (puzzle[i][j] === "#") {
             htmlString += "<td  class='filled'></td>";
          }
--- weatherPage.js
+++ weatherPage.js
@@ -1,13 +1,25 @@
+//default values
 var title = "Weather For This Week";
 var days = ["Sunny", "Rain", "Snow"];
-var feelsLike = ["65-70", "32-41", "9"]
-
+var feelsLike = ["65-70", "32-41", "9"];
 var temp = ["60", "40", "20"];
+
+//different city temps
+var gfTemp = ["65-70", "32-41", "9"];
+var bzTemp = ["55-73", "41-53", "34"];
+var btTemp = ["20-25", "30-36", "0"];
+
+//different city weather
+var gfWeather = ["Sunny", "Snow", "Snow"];
+var bzWeather = ["Sunny", "Sunny", "Rain"];
+var btWeather = ["Snow", "Rain", "Snow"];
+
 
 var heading = document.getElementById("header");
 
 heading.textContent = `${title}`;
 
+//for loop to print each day
 weatherHTML = ``;
 for (var i = 0; i < days.length; i++) {
 
@@ -15,7 +27,7 @@ for (var i = 0; i < days.length; i++) {
     <img src="${days[i]}.png"></img> <br/>
     Temperature: ${temp[i]} <br/> 
     Feels Like: ${feelsLike[i]}`;
-    
+
     var idName = `day-${i + 1}`;
     document.getElementById(idName).innerHTML = weatherHTML;
 
@@ -24,3 +36,5 @@ for (var i = 0; i < days.length; i++) {
 footerHTML = `These are not my logos, visit   
          <a href='https://windy.app/support/weather-symbols-and-icons.html'>Weather Logos</a> too see all the logos available.`;
 document.getElementById("foot").innerHTML = footerHTML;
+
+
