b48b6c6a76042f757c22e73ee8966e6ec29a95f1
KyleAlbro <kylealbro@charter.net>
2022-11-15 10:46:26 -0700

    12.2 done

--- html12/tutorial/bc_outline.js
+++ html12/tutorial/bc_outline.js
@@ -29,7 +29,7 @@
 
 window.addEventListener("load", makeOutline);
 
-function makeOutline(){
+function makeOutline() {
    var outline = document.getElementById("outline");
 
    var source = document.getElementById("doc");
@@ -44,16 +44,47 @@ function makeOutline(){
    createList(source, outlineList);
 }
 
-function createList(source, outlineList){
+function createList(source, outlineList) {
    var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
 
-   for (var n = source.firstChild; n !== null; n = n.nextSibling){
+   var prevLevel = 0;
+
+   var headNum = 0;
+   for (var n = source.firstChild; n !== null; n = n.nextSibling) {
       var headLevel = headings.indexOf(n.nodeName);
 
-      if(headLevel !== -1){
-          var listElem = document.createElement("li");
-          listElem.innerHTML = n.firstChild.nodeValue;
-          outlineList.appendChild(listElem);
+      if (headLevel !== -1) {
+         headNum++;
+         if (n.hasAttribute("id") === false) {
+            n.setAttribute("id", "head" + headNum);
+         }
+
+         var listElem = document.createElement("li");
+
+         var linkElem = document.createElement("a");
+         linkElem.innerHTML = n.innerHTML;
+         linkElem.setAttribute("href", "#" + n.id);
+
+         listElem.appendChild(linkElem);
+
+         if (headLevel === prevLevel) {
+            outlineList.appendChild(listElem);
+         }
+         else if (headLevel > prevLevel) {
+            var nestedList = document.createElement("ol");
+            nestedList.appendChild(listElem);
+            outlineList.lastChild.appendChild(nestedList);
+            outlineList = nestedList;
+         }
+         else {
+            var levelUp = prevLevel - headLevel;
+            for (var i = 1; i <= levelUp; i++) {
+               outlineList = outlineList.parentNode.parentNode;
+            }
+            outlineList.appendChild(listElem);
+         }
+
+         prevLevel = headLevel;
       }
    }
 }
