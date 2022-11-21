98273475705efe410148fd235632f6ad8d75e731
KyleAlbro <kylealbro@charter.net>
2022-11-21 13:23:32 -0700

    done

--- javaScriptObjectsLab-main/objects.js
+++ javaScriptObjectsLab-main/objects.js
@@ -1,3 +1,11 @@
+// //sandbox area
+// var person = {
+//   name: "Malcolm Reynolds",
+//   vessel: "Sernity"
+// }
+
+// document.getElementById("sandbox").textcontent = person.name + " " + person.vessel;
+
 // Joke Object
 var joke = {
   categories: [],
@@ -10,6 +18,11 @@ var joke = {
     "Chuck Norris graduated from preschool, high school, and college before he was even born.",
 };
 
+var dateGen = new Date(joke.created_at);
+document.getElementById("generated").textContent = 
+`This joke was generated on ${dateGen.toLocaleString()}`;
+
+document.getElementById("joke").textContent = `${joke.value}`;
 
 var movie = {
   title: "Die Hard",
@@ -20,6 +33,22 @@ var movie = {
   },
 };
 // --- Using movie object
+document.getElementById("title").textContent = movie.title;
+var castElements = document.querySelectorAll(".cast");
+for(var i = 0; i < movie.cast.length; i++){
+  castElements[i].textContent = movie.cast[i];
+}
+
+movie.genre = "Christmas";
+movie["rating"] = "R";
+
+document.getElementById("review").textContent = 
+`${movie.reviewer.revName} gives this movie a ${movie.reviewer.stars}
+stars and is his favorite ${movie.genre} movie. 
+It is rated ${movie.rating}.`;
+
+
+
 
 // ----
 var courses = {
@@ -36,3 +65,17 @@ var courses = {
 };
 
 // Using the Courses Object
+var courseElements = document.querySelectorAll(".course");
+var elementIndex = 0;
+for(k in courses.CSCI){
+  courseElements[elementIndex].textContent = 
+  `${k} -- ${courses.CSCI[k]}`;
+  elementIndex++;
+  // console.log(k);
+}
+for(k in courses.ITS){
+  courseElements[elementIndex].textContent = 
+  `${k} -- ${courses.ITS[k]}`;
+  elementIndex++;
+  // console.log(k);
+}
